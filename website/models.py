69f106635eed2e13bbf1aa650e3e86a06b31dc31
mauricedummel <maurice.duemmel@gmail.com>
2022-07-30 22:39:42 +0200

    Added    - Scrape in models.py with relationship to user    - scraping.html    - fixed logout not working in base.html    - fixed views.py to work with user authentication

--- website/models.py
+++ website/models.py
@@ -1,9 +1,23 @@
+from sqlalchemy.orm import relationship
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    scrapes = db.relationship("Scrape", backref="user", lazy=True)
+
+
+class Scrape(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    url = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    regex = db.Column(db.String(500))
+    search_string = db.Column(db.String(500))
+
--- website/views.py
+++ website/views.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .models import Scrape
+from flask_login import login_required, current_user
+from . import db
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+
+@views.route("/scrape", methods=['GET', 'POST'])
+@login_required
+def scrape():
+    if request.method == "POST":
+        scraping_regex = request.form.get("regex")
+        scraping_url = request.form.get("url")
+
+        new_scraping_request = Scrape(url=scraping_url, regex=scraping_regex)
+        db.session.add(new_scraping_request)
+        db.session.commit()
+    else:
+        return render_template("scraping.html", user=current_user)
